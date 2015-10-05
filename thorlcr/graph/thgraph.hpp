942258251d3ece11a0c4a249ec63c60306b797c5
Richard Chapman <rchapman@hpccsystems.com>
2015-10-05 15:25:48 +0100

    HPCC-13030 Add row tracing support to engines
    Implement row tracing for Thor

--- thorlcr/graph/thgraph.hpp
+++ thorlcr/graph/thgraph.hpp
@@ -74,7 +74,8 @@ enum msgids
     GraphInit,
     GraphEnd,
     GraphAbort,
-    GraphGetResult
+    GraphGetResult,
+    DebugRequest
 };
 
 interface ICodeContextExt : extends ICodeContext
@@ -709,6 +710,7 @@ public:
 //  virtual void getResult(size32_t & retSize, void * & ret, unsigned id);
 //  virtual void getLinkedResult(unsigned & count, byte * * & ret, unsigned id);
     virtual IEclGraphResults *evaluate(unsigned parentExtractSz, const byte * parentExtract);
+
 friend class CGraphElementBase;
 };
 
@@ -847,6 +849,7 @@ public:
     unsigned __int64 getOptUInt64(const char *opt, unsigned __int64 dft=0) { return (unsigned __int64)getOptInt64(opt, dft); }
 
     virtual void abort(IException *e);
+    virtual void debugRequest(CMessageBuffer &msg, const char *request) const { }
 
 //
     virtual void addCreatedFile(const char *file) { assertex(false); }
